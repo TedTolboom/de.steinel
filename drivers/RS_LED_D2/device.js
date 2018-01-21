81a33f9080d5ff050dcacca74084a6d73b8d9555
dT <dTNL.Homey@gmail.com>
2018-01-21 09:01:51 +0100

    Remove not supported Dim capability and update mobile card

--- drivers/RS_LED_D2/device.js
+++ drivers/RS_LED_D2/device.js
@@ -7,7 +7,6 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class SteinhelRSLEDD2 extends ZwaveDevice {
 	onMeshInit() {
 		this.registerCapability('onoff', 'BASIC');
-		this.registerCapability('dim', 'SWITCH_MULTILEVEL');
 		this.registerCapability('measure_luminance', 'SENSOR_MULTILEVEL');
 		this.registerCapability('alarm_motion', 'NOTIFICATION');
 	}
